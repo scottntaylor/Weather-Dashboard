0c859627ca0eb8ae8261103033fd63eefa48e772
Scott Taylor <scottntaylor21@gmail.com>
2019-10-26 09:44:01 -0700

    Data displaying

--- script.js
+++ script.js
@@ -12,19 +12,12 @@ $("#go").on("click", function (event) {
     event.preventDefault();
     var city = $("#search").val();
     console.log(city);
+    getWeather(city);
+});
+
+
+function getWeather(city) {
     var queryURL = `${proxy}api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
-
-    function getUV(lon, lat) {
-        var UVAPI = `${proxy}api.openweathermap.org/data/2.5/uvi/forecast?appid=${apiKey}&lat=${lat}&lon=${lon}`
-
-        $.ajax({
-            url: UVAPI,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response[0].value);
-            var UV = response[0].value;
-        }
-        )}
 
     $.ajax({
         url: queryURL,
@@ -33,33 +26,60 @@ $("#go").on("click", function (event) {
         console.log(response);
         console.log(response.name);
 
-        var cityName = response.name;
-        var date = moment(new Date()).format("MM/DD/YYYY");
-        var temp = response.main.temp;
-        var humidity = response.main.humidity;
-        var wind = response.wind.speed;
+        displayWeatherData(response, city);
+
         var lon = response.coord.lon;
         var lat = response.coord.lat;
         console.log(lon, lat);
+
         getUV(lon, lat);
         console.log(getUV);
     })
-})
+}
+
+function getUV(lon, lat) {
+    var UVAPI = `${proxy}api.openweathermap.org/data/2.5/uvi/forecast?appid=${apiKey}&lat=${lat}&lon=${lon}`
+
+    $.ajax({
+        url: UVAPI,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response[0].value);
+        var UV = response[0].value;
+    })
+}
 
 
 //On Click Function
-function searchCity(response) {
+// function searchCity(response) {
 
-    //Grap city from API
-}
-searchCity();
-function displayCityData() {
+// //     //Grap city from API
+// // }
+// searchCity();
+
+function displayWeatherData(response, cityName) {
+
+    var cityName = response.name;
+    var date = moment(new Date()).format("MM/DD/YYYY");
+    var temp = response.main.temp;
+    var humidity = response.main.humidity;
+    var wind = response.wind.speed;
+
     // Pull data from API
-    $("#cityndate").text(response.name);
+    $("#cityndate").text(cityName + " " + date);
+    var iconcode = response.weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    $('#wicon').attr('src', iconurl);
+    $("#temp").text(temp);
+    $("#humidity").text(humidity);
+    $("#wind").text(wind);
+    // $("#UV").text(UV);
 
 }
 
-
+function fiveDayForecast() {
+    //To Do
+}
 // function addCity(response) {
 //     $("#cities").append("<div>" + city + "<div>");
 
